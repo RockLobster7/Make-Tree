1eda9169079becc41bf8697e9745b23664c9af59
Mario Cueva <mario.cueva.au@gmail.com>
2017-10-01 20:25:17 +1100

    reformatted

--- Make-Tree.js
+++ Make-Tree.js
@@ -16,14 +16,13 @@ let categories = [
 let makeTree = (categories, parent) => {
     let node = {}
     categories
-    .filter(c => c.parent === parent)
-    .forEach(c =>
-    node[c.id] =  makeTree(categories, c.id))
+        .filter(c => c.parent === parent)
+        .forEach(c =>
+            node[c.id] = makeTree(categories, c.id))
     return node;
 }
 
 console.log(
     JSON.stringify(
-        makeTree(categories, null)
-    ,null, 2)
+        makeTree(categories, null), null, 2)
 );
